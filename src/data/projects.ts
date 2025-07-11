e89b80b526cf82e232d0fc5044a37c04e9645571
muhammedrafi-kp <rafikpmty@gmail.com>
2025-07-11 21:08:46 +0530

    add : aadhaar ocr project, new cv

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -1,6 +1,7 @@
 import type { Project } from '../types';
 import greenora from '../assets/greenora.png';
 import stepEx from '../assets/stepex.png';
+import aadharOCR from "../assets/aadharocr.png";
 
 const projects: Project[] = [
 
@@ -23,8 +24,17 @@ const projects: Project[] = [
     liveUrl: 'https://stepex.mhdrafi.online/',
     sourceUrl: 'https://github.com/MuhammedRafikp/stepex-ecommerce',
   },
+  {
+    id: 3,
+    title: 'Aadhaar OCR System',
+    description: 'A TypeScript-based client-server app that extracts Aadhaar card data from images using Google Cloud Vision API and custom parsing logic.',
+    image: aadharOCR,
+    tags: ["React.js", "TypeScript", "Tailwind CSS", "Node.js", "Express.js", "Multer", "Google Cloud Vision API", "Vite", "Axios"],
+    liveUrl: 'https://aadhaar-ocr.mhdrafi.online/', // replace with actual live URL if deployed
+    sourceUrl: 'https://github.com/muhammedrafi-kp/aadhaar-ocr-system.git', // replace with your actual GitHub repo
+  }
   // {
-  //   id: 3,
+  //   id: 4,
   //   title: 'Weather Dashboard',
   //   description: 'Real-time weather application with forecast data, location search, and interactive maps.',
   //   image: 'https://images.pexels.com/photos/1162251/pexels-photo-1162251.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
